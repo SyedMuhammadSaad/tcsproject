639737a5d588e36dd8ac67eddad280c20117a66a
Saad <Saad@Acecom>
2016-06-28 14:00:31 +0500

    Constant variables added for db connectivity in config.php

---
+++ app/config.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Config.php is configuring the database file
+ */
+/**
+ * Database name
+ */
+define('DB_NAME', 'tcsproject');
+/**
+ * Database root
+ */
+define('DB_USER', 'root');
+/**
+ * Database password
+ */
+define('DB_PASSWORD', '');
+/**
+ * Database host
+ */
+define("DB_HOST", 'localhost');
